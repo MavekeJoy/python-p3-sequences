93f6de30ad255796f418032fa335fb648882b873
JoyMaveke <Mavekejoy@gmail.com>
2024-08-29 12:57:27 +0300

    Initial Commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+  
+    if length <= 0:
+        print([])
+        return
+    
+    if length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+    
+   
+    fib_seq = [0, 1]
+    
+    for _ in range(2, length):
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
+    
+   
+    print(fib_seq)
+
+
+print_fibonacci(10)
+
+    
